e470b21b196fe1fb064e5c08f4ab362749ddb5dc
raellff <isra.fdf@gmail.com>
2025-06-05 04:00:26 +0000

    modified:   pages/api/lookup.js

--- pages/api/lookup.js
+++ pages/api/lookup.js
@@ -1,6 +1,6 @@
 import Airtable from "airtable";
 
-const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base("appWZS2nyZQ9vuQM7");
+const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base("42895S736PCRpatvEux3XJsaBjGjX.58db788f5e6741c2fb9ab93ca2264c553e4135387a2f71894c13412e3777a8d2");
 
 export default async function handler(req, res) {
   console.log("Requisição lookup, query:", req.query);
